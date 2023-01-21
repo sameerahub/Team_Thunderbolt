1c25d7ee4d7bcb3b055a357fa2e62e9551a9289d
Yomindu Rajasuriya <51852251+Yomindu@users.noreply.github.com>
2023-01-22 03:34:31 +0530

    done

--- index.php
+++ index.php
@@ -1,1 +1,57 @@
 
+<?php 
+
+?>
+
+<html>  
+<head>  
+    <title>PHP login system</title>  
+    <link rel = "stylesheet" type = "text/css" href = "style.css">   
+</head>  
+<body> 
+     <img src="SLTC.jpg" 
+     width="100%" 
+     height="100%" />
+    <div class="form-wrapper">
+    <div id = "frm">  
+        <h1>Login</h1>  
+        <form name="f1" action = "authentication.php" onsubmit = "return validation()" method = "POST">  
+            <p>  
+                <label> UserName: </label>  
+                <input type = "text" id ="user" name  = "user" />  
+            </p>  
+            <p>  
+                <label> Password: </label>  
+                <input type = "password" id ="pass" name  = "pass" />  
+            </p>  
+            <p>     
+                <input type =  "submit" id = "btn" value = "Login" />  
+            </p>  
+        </form>  
+    </div>  
+    
+    <script>  
+            function validation()  
+            {  
+                var id=document.f1.user.value;  
+                var ps=document.f1.pass.value;  
+                if(id.length=="" && ps.length=="") {  
+                    alert("User Name and Password fields are empty!");  
+                    return false;  
+                }  
+                else  
+                {  
+                    if(id.length=="") {  
+                        alert("User Name is empty!");  
+                        return false;  
+                    }   
+                    if (ps.length=="") {  
+                    alert("Password field is empty!");  
+                    return false;  
+                    }  
+                }                             
+            }  
+        </script> 
+</div>
+</body>     
+</html>
