7cda1fd9637e88a3a9c0025095f66c310987c153
pabasariG <121004185+pabasariG@users.noreply.github.com>
2023-01-29 02:03:55 +0530

    Add files via upload

---
+++ New folder (8)/UIlogin.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <title>Attendance System</title>
+  <!-- plugins:css -->
+  <link rel="stylesheet" href="../../vendors/ti-icons/css/themify-icons.css">
+  <link rel="stylesheet" href="../../vendors/base/vendor.bundle.base.css">
+  <!-- endinject -->
+  <!-- plugin css for this page -->
+  <!-- End plugin css for this page -->
+  <!-- inject:css -->
+  <link rel="stylesheet" href="../../css/style.css">
+  <!-- endinject -->
+</head>
+
+<body>
+  <div class="container-scroller">
+    <div class="container-fluid page-body-wrapper full-page-wrapper">
+      <div class="content-wrapper d-flex align-items-center auth px-0">
+        <div class="row w-100 mx-0">
+          <div class="col-lg-4 mx-auto">
+            <div class="auth-form-light text-left py-5 px-4 px-sm-5">
+                <h4>ATTENDANCE SYSTEM</h4>
+              <h6 class="font-weight-light">Sign in to continue.</h6>
+              <form class="pt-3">
+                <div class="form-group">
+                  <input type="email" class="form-control form-control-lg" id="exampleInputEmail1" placeholder="Username">
+                </div>
+                <div class="form-group">
+                  <input type="password" class="form-control form-control-lg" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <div class="mt-3">
+                  <a class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" href="../dashboard/dashboard.php">SIGN IN</a>
+                    <a class="btn btn-block btn-default btn-lg font-weight-medium auth-form-btn" href="../barcode/barcode.php">SIGN IN USING BARCODE</a>
+                </div>
+                <div class="my-2 d-flex justify-content-between align-items-center">
+                  <div class="form-check">
+                    <label class="form-check-label text-muted">
+                      <input type="checkbox" class="form-check-input">
+                      Keep me signed in
+                    </label>
+                  </div>
+                  <a href="#" class="auth-link text-black">Forgot password?</a>
+                </div>
+                <div class="text-center mt-4 font-weight-light">
+                  Don't have an account? <a href="../register/register.php" class="text-primary">Register</a>
+                </div>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+      <!-- content-wrapper ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+  <!-- plugins:js -->
+  <script src="../../vendors/base/vendor.bundle.base.js"></script>
+  <!-- endinject -->
+  <!-- inject:js -->
+  <script src="../../js/off-canvas.js"></script>
+  <script src="../../js/hoverable-collapse.js"></script>
+  <script src="../../js/template.js"></script>
+  <script src="../../js/todolist.js"></script>
+  <!-- endinject -->
+</body>
+
+</html>
---
+++ New folder (8)/UIstudent.php
@@ -0,0 +1,85 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<?php include '../head.php'; ?>
+<body>
+  <div class="container-scroller">
+    <!-- partial:partials/_navbar.html -->
+    <?php include '../navbar.php'; ?>
+    <!-- partial -->
+    <div class="container-fluid page-body-wrapper">
+      <!-- partial:partials/_sidebar.html -->
+      <?php include '../sidebar.php'; ?>
+      <!-- partial -->
+      <div class="main-panel">
+        <div class="content-wrapper">
+          <div class="row">
+            <div class="col-md-12 grid-margin">
+              <div class="d-flex justify-content-between align-items-center">
+                  <div>
+                      <button type="button" class="btn btn-primary btn-icon-text btn-flat btn-sm" data-toggle="modal" data-target="#add-student">
+                          <i class="ti-plus btn-icon-prepend"></i>Add Student Information
+                      </button> 
+                  </div>
+                <div>
+                <!--    <button type="button" class="btn btn-primary btn-icon-text btn-rounded">
+                      <i class="ti-clipboard btn-icon-prepend"></i>Report
+                    </button> -->
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="row">
+            <div class="col-md-12 grid-margin stretch-card">
+              <div class="card">
+                <div class="card-body">
+                  <p class="card-title text-md-center text-xl-left">Student Information</p>
+                  <div class=" flex-wrap justify-content-between justify-content-md-center justify-content-xl-between align-items-center">
+                      <table id="example1" class="table table-striped" >
+                          <thead style="font-size:10px">
+                              <tr>
+                                  <th>ID</th>
+                                  <th>Student Barcode</th>
+                                  <th>Full Name</th>
+                                  <th>Contact</th>
+                                  <th>Course</th>
+                                  <th>Year Level</th>
+                                  <th></th>
+                              </tr>
+                          </thead>
+                          <tbody>
+                              <tr>
+                                  <td>001</td>
+                                  <td>2019</td>
+                                  <td>2019</td>
+                                  <td>2019</td>
+                                  <td>2019</td>
+                                  <td>2019</td>
+                                  <td><button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#edit-student">Edit</button></td>
+                              </tr>
+                          </tbody>
+                      </table>
+                  </div>  
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+          <?php include '../modals.php'; ?>
+        <!-- content-wrapper ends -->
+        <!-- partial:partials/_footer.html -->
+<?php include '../footer.php'; ?>
+        <!-- partial -->
+      </div>
+      <!-- main-panel ends -->
+    </div>
+    <!-- page-body-wrapper ends -->
+  </div>
+  <!-- container-scroller -->
+
+<?php include '../scripts.php'; ?>
+</body>
+
+</html>
+
